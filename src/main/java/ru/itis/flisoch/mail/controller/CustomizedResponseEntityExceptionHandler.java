dd1bd80dd6eadb67b59f0bb36d8c4502ed8d39b4
flisoch <flisochcs@gmail.com>
2019-05-21 17:46:19 +0300

    add resourceNotFound exception

--- src/main/java/ru/itis/flisoch/mail/controller/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/ru/itis/flisoch/mail/controller/CustomizedResponseEntityExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.itis.flisoch.mail.exception.ExceptionDetails;
+import ru.itis.flisoch.mail.exception.LogicalException;
+import ru.itis.flisoch.mail.exception.ResourceNotFoundException;
 import ru.itis.flisoch.mail.exception.UserAlreadyExistsException;
 
 import java.time.LocalDateTime;
@@ -24,6 +26,24 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         ExceptionDetails errorDetails = new ExceptionDetails(LocalDateTime.now(), exception.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(LogicalException.class)
+    public final ResponseEntity<ExceptionDetails> handleLogicalException(
+            UserAlreadyExistsException exception,
+            WebRequest request) {
+        ExceptionDetails errorDetails = new ExceptionDetails(LocalDateTime.now(), exception.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionDetails> handleResourseNotFoundException(
+            UserAlreadyExistsException exception,
+            WebRequest request) {
+        ExceptionDetails errorDetails = new ExceptionDetails(LocalDateTime.now(), exception.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
 }
